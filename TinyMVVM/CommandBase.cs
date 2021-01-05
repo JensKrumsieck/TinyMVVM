6ac12c7724a5368c75520bef6b9eb82f65dab2a7
Jens Krumsieck <jenso666@live.de>
2021-01-05 15:42:16 +0100

    introduce relaycommand<T>

---
+++ TinyMVVM/CommandBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace TinyMVVM
+{
+    public abstract class CommandBase : ICommand
+    {
+        /// <inheritdoc/>
+        bool ICommand.CanExecute(object parameter) => CanExecute(parameter);
+
+        /// <inheritdoc/>
+        void ICommand.Execute(object parameter) => Execute(parameter);
+
+        /// <inheritdoc/>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// Raises <see cref="ICommand.CanExecuteChanged"/>
+        /// </summary>
+        protected virtual void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+        /// <summary>
+        /// internal handling of <see cref="ICommand.Execute"/>
+        /// </summary>
+        /// <param name="parameter"></param>
+        protected abstract void Execute(object parameter);
+
+        /// <summary>
+        /// internal handling of <see cref="ICommand.CanExecute"/>
+        /// </summary>
+        /// <param name="parameter"></param>
+        protected abstract bool CanExecute(object parameter);
+    }
+}

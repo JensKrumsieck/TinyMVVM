ccf50f8c8ab04a2eb7a1fd3c48be051ca6566982
Jens Krumsieck <jenso666@live.de>
2021-01-23 11:53:42 +0100

    add ListIItemViewModel

---
+++ TinyMVVM/ListItemViewModel.cs
@@ -0,0 +1,26 @@
+﻿namespace TinyMVVM
+{
+    /// <summary>
+    /// Represents a ListItem
+    /// To be used with <see cref="ListingViewModel{T}"/>
+    /// </summary>
+    public class ListItemViewModel<T> : BaseViewModel where T : class
+    {
+
+        /// <summary>
+        /// The Parent View Model of Type <see cref="ListingViewModel{T}"/>
+        /// </summary>
+        public ListingViewModel<T> Parent { get; }
+
+        /// <summary>
+        /// Indicates if this Item is selected
+        /// </summary>
+        public bool IsSelected => Parent?.SelectedItem == this;
+
+        public ListItemViewModel(ListingViewModel<T> parent)
+        {
+            Parent = parent;
+            Parent.SelectedIndexChanged += (s, e) => OnPropertyChanged(nameof(IsSelected));
+        }
+    }
+}
